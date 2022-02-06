661dd7a89f0d6c9e659e5656e0a065100781a9d9
Rubens de Andrade <rubensaz14@gmail.com>
2022-02-06 12:57:48 -0300

    feat(savings): added currency mask to amount input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxMaskModule } from 'ngx-mask';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    NgxMaskModule.forRoot(),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/savings/components/components.module.ts
+++ src/app/savings/components/components.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
 
 import { AmountInputComponent } from './amount-input/amount-input.component';
 import { MonthYearPickerComponent } from './month-year-picker/month-year-picker.component';
 
 @NgModule({
   declarations: [AmountInputComponent, MonthYearPickerComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskModule],
   exports: [AmountInputComponent, MonthYearPickerComponent],
 })
 export class ComponentsModule {}
